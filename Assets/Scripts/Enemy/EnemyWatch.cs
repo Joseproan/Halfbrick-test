c24efaa7996527df0bc6eed6a9d8c9ba7cc383b8
Josep <josepsamc@gmail.com>
2024-10-28 03:32:58 +0100

    2d NavMesh

--- Assets/Scripts/Enemy/EnemyWatch.cs
+++ Assets/Scripts/Enemy/EnemyWatch.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class EnemyWatch : MonoBehaviour
 {
@@ -10,10 +11,23 @@ public class EnemyWatch : MonoBehaviour
     private bool explosion;
 
     [SerializeField] private TextMeshProUGUI timerText;
+
+    private NavMeshAgent agent;
+    private GameObject player;
+    private Transform playerPos;
+    [SerializeField] private float speed;
+
+    public float minDistance = 2.0f;
     // Start is called before the first frame update
     void Start()
     {
         explosionTimer = timeBetweenExplosion;
+        agent = GetComponent<NavMeshAgent>();
+        player = GameObject.FindWithTag("Player");
+        playerPos = player.transform;
+        agent.updateRotation = false;
+        agent.updateUpAxis = false;
+        agent.speed = speed;
     }
 
     // Update is called once per frame
@@ -31,6 +45,22 @@ public class EnemyWatch : MonoBehaviour
             explosionTimer -= Time.deltaTime;
         }
         
-        
+        FollowPlayer();
+    }
+
+    void FollowPlayer()
+    {
+        float distanceToPlayer = Vector3.Distance(transform.position, playerPos.position);
+
+        // Si la distancia es mayor que el mínimo, el enemigo persigue al jugador
+        if (distanceToPlayer > minDistance)
+        {
+            agent.SetDestination(playerPos.position);
+        }
+        else
+        {
+            // Detiene al agente si está dentro de la distancia mínima
+            agent.SetDestination(this.transform.position);
+        }
     }
 }
